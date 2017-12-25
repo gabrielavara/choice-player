5911e6fa0c087afc88c7623045707df121e6005c
Gabor Nagy <gabrielavara@gmail.com>
2017-12-25 23:31:55 +0100

    Move file in thread

--- src/main/java/com/gabrielavara/choiceplayer/util/FileMover.java
+++ src/main/java/com/gabrielavara/choiceplayer/util/FileMover.java
@@ -1,11 +1,11 @@
 package com.gabrielavara.choiceplayer.util;
 
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.stream.IntStream;
 
 import com.gabrielavara.choiceplayer.views.PlaylistItemView;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,23 +24,38 @@ public abstract class FileMover {
         log.info("Move file to {}", getTarget());
         playlistUtil.getCurrentlyPlayingPlaylistItemView().ifPresent(item -> {
             playlistUtil.getNextPlaylistItemView().ifPresent(playlistUtil::select);
-            try {
-                playlistItemViews.removeAll(item);
-                moveFile(item);
-                IntStream.range(0, playlistItemViews.size()).forEach(i -> playlistItemViews.get(i).setIndex(i + 1));
-            } catch (IOException e) {
-                playlistItemViews.add(item.getIndex() - 1, item);
-                log.error("Could not move {} to {} because {}", item.getMp3(), getTarget(), e.getMessage());
-                sortPlaylist();
-            }
+            playlistItemViews.removeAll(item);
+            startMoveTask(item);
         });
+    }
+
+    private void startMoveTask(PlaylistItemView item) {
+        Task<Void> task = createMoveTask(item);
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    private Task<Void> createMoveTask(PlaylistItemView item) {
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws IOException {
+                moveFile(item);
+                return null;
+            }
+        };
+        task.setOnSucceeded(e -> {
+            IntStream.range(0, playlistItemViews.size()).forEach(i -> playlistItemViews.get(i).setIndex(i + 1));
+            log.info("Successfully moved to {}", getTarget());
+        });
+        task.setOnFailed(e -> {
+            playlistItemViews.add(item.getIndex() - 1, item);
+            log.error("Could not move to {}: {}", getTarget(), item.getMp3());
+        });
+        return task;
     }
 
     protected abstract String getTarget();
 
     protected abstract void moveFile(PlaylistItemView itemView) throws IOException;
-
-    private void sortPlaylist() {
-        playlistItemViews.sort(Comparator.comparingInt(PlaylistItemView::getIndex));
-    }
 }
