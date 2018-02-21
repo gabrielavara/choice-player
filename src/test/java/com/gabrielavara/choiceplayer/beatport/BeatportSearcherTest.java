9cef651ef4751a5b1b0bb6351e3dd0a45c77aa3c
Gabor8 Nagy <gabor8.nagy.ext@evosoft.com>
2018-02-21 11:30:20 +0100

    Fix BeatportSearcherTest

--- src/test/java/com/gabrielavara/choiceplayer/beatport/BeatportSearcherTest.java
+++ src/test/java/com/gabrielavara/choiceplayer/beatport/BeatportSearcherTest.java
@@ -12,13 +12,12 @@ import org.junit.Test;
 import com.gabrielavara.choiceplayer.dto.Mp3;
 
 public class BeatportSearcherTest {
-
     @Test
     public void testSearch() {
         // given
         Mp3 mp3 = new Mp3();
-        mp3.setAlbum("Another Chance");
-        mp3.setArtist("Above & Beyond pres. Oceanlab");
+        mp3.setAlbum("Amun / Meta");
+        mp3.setArtist("Ilan Bluestone & Jason Ross");
 
         // when
         Optional<BeatportAlbum> album = BeatportSearcher.search(mp3);
@@ -30,13 +29,12 @@ public class BeatportSearcherTest {
 
     private BeatportAlbum getExpectedAlbum() {
         BeatportAlbum expected = new BeatportAlbum();
-        expected.setTitle("Another Chance");
-        List<String> expectedArtists = asList("OceanLab", "Above & Beyond");
+        expected.setTitle("Amun / Meta");
+        List<String> expectedArtists = asList("Ilan Bluestone", "Jason Ross");
         expected.setArtists(expectedArtists);
-        BeatportTrack track1 = new BeatportTrack("1", expectedArtists, "Another Chance", "Original Mix");
-        BeatportTrack track2 = new BeatportTrack("2", expectedArtists, "Another Chance", "Above & Beyond Club Mix");
-        BeatportTrack track3 = new BeatportTrack("3", expectedArtists, "Another Chance", "Above & Beyond Club Edit");
-        expected.setTracks(asList(track1, track2, track3));
+        BeatportTrack track1 = new BeatportTrack("1", expectedArtists, "Amun", "Original Mix");
+        BeatportTrack track2 = new BeatportTrack("2", expectedArtists, "Meta", "Original Mix");
+        expected.setTracks(asList(track1, track2));
         return expected;
     }
 
