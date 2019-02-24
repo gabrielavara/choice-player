4ea47c5590caa4898d2243f5ab3bdc687511f488
Gabor Nagy <gabrielavara@gmail.com>
2019-02-24 01:09:56 +0100

    Fix for changing selection with mouse

--- src/main/java/com/gabrielavara/choiceplayer/playlist/PlaylistSelectionChangedListener.java
+++ src/main/java/com/gabrielavara/choiceplayer/playlist/PlaylistSelectionChangedListener.java
@@ -7,14 +7,19 @@ import com.gabrielavara.choiceplayer.util.Opinion;
 import com.gabrielavara.choiceplayer.views.PlaylistItemView;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import lombok.Setter;
 
 import java.util.Optional;
 
 public class PlaylistSelectionChangedListener implements ChangeListener<PlaylistItemView> {
-    @Setter
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private Optional<Opinion> opinion = Optional.empty();
+    private boolean opinionSet;
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public void setOpinion(Optional<Opinion> opinion) {
+        this.opinion = opinion;
+        opinionSet = true;
+    }
 
     @Override
     public void changed(ObservableValue<? extends PlaylistItemView> observable, PlaylistItemView oldValue, PlaylistItemView newValue) {
@@ -25,6 +30,10 @@ public class PlaylistSelectionChangedListener implements ChangeListener<Playlist
     }
 
     private void changed(Mp3 oldValue, Mp3 newValue, PlaylistItemView oldItemView) {
+        if (!opinionSet) {
+            opinion = Optional.empty();
+        }
         Messenger.send(new SelectionChangedMessage(newValue, oldValue, oldItemView, true, opinion));
+        opinionSet = false;
     }
 }
